b8cd472733a01b5c22072b9b9fe6c6c417f30509
ErikMN <yubnut@gmail.com>
2021-11-29 21:03:44 +0100

    Use "main" for main branch name
    On git versions older than 2.28 "git init -b <name>" does not work: use
    "git init && git checkout -b <name>"

--- init_cproject.sh
+++ init_cproject.sh
@@ -9,6 +9,19 @@ fi
 BASEDIR=$(dirname "$0")
 # echo "$BASEDIR"
 
+SUPPORTED="2.28.0"
+GIT_VERSION=$(git --version | cut -d' ' -f3)
+
+# Initialize a git repo with an inital commit (git < 2.28.0 does not support 'git init -b <name>'):
+GIT_INIT_REPO() {
+  if (( $(echo "$GIT_VERSION $SUPPORTED" | awk '{print ($1 >= $2)}') )); then
+    git init -b main &&
+  else
+    git init && git checkout -b main &&
+  fi
+  git add -A && git commit -m 'Initial commit'
+}
+
 echo "*** Initializing C project '$APP' in this folder"
 echo
 
@@ -17,7 +30,7 @@ mkdir -p $APP && cp -r $BASEDIR/c_src/. ./$APP/ && mkdir -p ./$APP/.vscode &&
 cp $BASEDIR/vscode/*.json ./$APP/.vscode/ &&
 sed -i.bak "s/xxxxxxxxx/$APP/g" ./$APP/Makefile ./$APP/.gitignore &&
 cd ./$APP/ && rm *.bak .*.bak &&
-git init && git add -A && git commit -m 'Initial commit' && cd ..
+GIT_INIT_REPO && cd ..
 code ./$APP &&
 
 echo &&
