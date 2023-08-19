4da14ef51cab2f43fff8b8d8b5f30184047c15f7
ErikMN <yubnut@gmail.com>
2023-08-19 17:11:49 +0200

    Add meson template first draft

---
+++ share/vscode_init/tpl_c_meson/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <libgen.h>
+#include <errno.h>
+#include <string.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <assert.h>
+#include <stdint.h>
+#include <math.h>
+
+#ifdef DEBUG
+#define DBUG(x) x
+#else
+#define DBUG(x)
+#endif
+
+#define print_debug(...) DBUG(printf(__VA_ARGS__))
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(0 [x]))
+#endif
+
+int
+main(int argc, char *argv[])
+{
+  (void)argc;
+  (void)argv;
+
+  printf("\nHello World!\n");
+  printf("%s:%d\n", __FILE__, __LINE__);
+
+  print_debug("\n*** Development build\n");
+  print_debug("Build: %s %s\n", __DATE__, __TIME__);
+
+  return EXIT_SUCCESS;
+}
